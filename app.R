bcf93ff44d6237e40422aa6b89b7f05a359bfbb4
peterrosenquist80 <peter.rosenquist@wustl.edu>
2018-12-02 15:34:34 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,13 +1,3 @@
-## ------Text Comparison App------
-##   This app is designed to collect data from surveys where the participant is 
-## shown a set of texts and is asked a question about those texts.  The provided 
-## example datasets show the participant 20 sets of IMDB movie reviews and 
-## ask the participant to select the most positive review.  Data collected from
-## this app could be used with sentiment analysis alogrithms to determine the 
-## degree of positivity expressed by English emotion words.  I codeveloped this
-## app as my final project for my graduate level Political Science course,
-## Applied Statistical Programming. 
-
 ## install 'shiny', 'DT', and 'shinythemes' packages as needed
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(shiny, DT, shinythemes)
